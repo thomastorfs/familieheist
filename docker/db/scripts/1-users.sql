b07202108760e3183d95775c2953acd6e064ff63
Thomas Torfs <thomas.torfs@gmail.com>
2021-09-05 10:59:43 +0200

    Add keycloak server

--- docker/db/scripts/1-users.sql
+++ docker/db/scripts/1-users.sql
@@ -1,1 +1,3 @@
 CREATE USER familieheist WITH PASSWORD 'familieheist';
+
+CREATE USER keycloak WITH PASSWORD 'keycloak';
--- docker/db/scripts/2-schema.sql
+++ docker/db/scripts/2-schema.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE familieheist;
-
 GRANT ALL PRIVILEGES ON DATABASE familieheist TO familieheist;
+
+CREATE DATABASE keycloak;
+GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
