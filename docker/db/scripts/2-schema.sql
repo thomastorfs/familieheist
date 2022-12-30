793b6a98b7d2ab0c8597f8aafeb84d2d04230a14
Thomas Torfs <thomas.torfs@gmail.com>
2022-12-31 00:14:49 +0100

    fix postgresql docker and liquibase issues

--- docker/db/scripts/2-schema.sql
+++ docker/db/scripts/2-schema.sql
@@ -1,5 +1,9 @@
 CREATE DATABASE familieheist;
 GRANT ALL PRIVILEGES ON DATABASE familieheist TO localdev;
+\c familieheist;
+GRANT ALL ON SCHEMA public TO localdev;
 
 CREATE DATABASE keycloak;
 GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+\c keycloak;
+GRANT ALL ON SCHEMA public TO keycloak;
