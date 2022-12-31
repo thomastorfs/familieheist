e9fad8c62aca8a27ab10194312526f592be992ae
Thomas Torfs <thomas.torfs@gmail.com>
2022-12-31 22:14:04 +0100

    add basic setup for jwt authentication

---
+++ backend/src/main/java/be/familieheist/web/security/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package be.familieheist.web.security;
+
+import be.familieheist.web.system.user.Role;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfiguration {
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, Converter<Jwt, Mono<AbstractAuthenticationToken>> jwtAuthenticationConverter) {
+        http
+            .authorizeExchange()
+            .pathMatchers("/familieheist/**")
+            .hasAnyAuthority(Arrays.stream(Role.values()).map(Enum::toString).collect(Collectors.joining()))
+            .anyExchange().authenticated()
+            .and()
+            .oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter);
+        return http.build();
+    }
+
+    @Bean
+    public JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+        return jwtGrantedAuthoritiesConverter;
+    }
+
+    @Bean
+    public Converter<Jwt, Flux<GrantedAuthority>> reactiveJwtGrantedAuthoritiesConverter(JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter) {
+        return new ReactiveJwtGrantedAuthoritiesConverterAdapter(jwtGrantedAuthoritiesConverter);
+    }
+
+    @Bean
+    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter(Converter<Jwt, Flux<GrantedAuthority>> reactiveJwtGrantedAuthoritiesConverter) {
+        ReactiveJwtAuthenticationConverter jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(reactiveJwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+}
