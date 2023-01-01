c43b7319e333859527e9d51b85903beb0dd25a5c
Thomas Torfs <thomas.torfs@gmail.com>
2023-01-01 15:36:03 +0100

    add openapi schema definition for pageparts

--- backend/src/main/java/be/familieheist/web/content/page/PagepartDTO.java
+++ backend/src/main/java/be/familieheist/web/content/page/PagepartDTO.java
@@ -1,13 +1,19 @@
 package be.familieheist.web.content.page;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record PagepartDTO(
+    @Schema(description = "Identifier for a Content Pagepart", example = "95cea6d9-7698-4a15-8751-ed5512f69bca")
     String id,
+    @Schema(description = "The title of the Content Pagepart", example = "Introduction: Uncle Bob")
     String title,
+    @Schema(description = "The description of the Content Pagepart", example = "This is how Uncle Bob's life started")
     String description,
+    @Schema(description = "The type of the Content Pagepart")
     PagepartType type,
+    @Schema(description = "The position  of the Content Pagepart within the Content Page")
     Integer position
 ) implements Comparable<PagepartDTO> {
     @Override
