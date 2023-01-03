84d111d581148ce5ce08a81977cbdd8c3a860b90
Thomas Torfs <thomas.torfs@gmail.com>
2023-01-03 16:56:12 +0100

    remove unused to dbo method in pagepart update command

--- backend/src/main/java/be/familieheist/web/content/page/part/PagepartUpdateCommandDTO.java
+++ backend/src/main/java/be/familieheist/web/content/page/part/PagepartUpdateCommandDTO.java
@@ -3,7 +3,6 @@ package be.familieheist.web.content.page.part;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.Optional;
-import java.util.UUID;
 
 public record PagepartUpdateCommandDTO(
     @Schema(description = "The title of the Content Pagepart", example = "Introduction: Uncle Bob")
@@ -15,12 +14,4 @@ public record PagepartUpdateCommandDTO(
     @Schema(description = "The position of the Content Pagepart within the Content Page")
     Integer position
 ) {
-    public PagepartDBO toPagepartDBO() {
-        PagepartDBO pagepartDBO = new PagepartDBO();
-        pagepartDBO.setTitle(title);
-        description.ifPresent(pagepartDBO::setDescription);
-        pagepartDBO.setType(type);
-        pagepartDBO.setPosition(position);
-        return pagepartDBO;
-    }
 }
